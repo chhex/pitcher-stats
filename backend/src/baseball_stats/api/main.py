9042e9e2063ee1f9f8060d302a3aab99540dc2be
Christoph Henrici <chhenricigmail.com>
2026-08-30 17:46:46 +0200

    CORS fix

--- backend/src/baseball_stats/api/main.py
+++ backend/src/baseball_stats/api/main.py
@@ -9,8 +9,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://.*\.vercel\.app",
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_methods=["*"],
     allow_headers=["*"],
 )
